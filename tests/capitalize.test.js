59378bb5a4109b8069d3b93d4fda5f8f5e7e21b8
Aleksandr <59273652+ALezhnin87@users.noreply.github.com>
2022-07-18 22:49:48 +0300

    Update capitalize.test.js
    Replaced manual assertions with the use of the assert module.

--- tests/capitalize.test.js
+++ tests/capitalize.test.js
@@ -1,14 +1,10 @@
 // нажмите кнопку run чтобы запустить тесты
 // попробуйте изменять код функции / тестов, запуская проверки заново
 
+import { strict as assert } from 'assert';
 import { capitalize } from '../src/capitalize.js';
 
-if (capitalize('hello') !== 'Hello') {
-  throw new Error('Функция работает неверно!');
-}
+assert(capitalize('') === '');
+assert(capitalize('hello') === 'Hello');
 
-if (capitalize('') !== '') {
-  throw new Error('Функция работает неверно!');
-}
-
-console.log('Все тесты пройдены!');
+console.log('All tests have passed!');
